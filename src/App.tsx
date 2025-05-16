7af5cc4f6406414675947d10cd7b46098e70822b
Ahnaf Irfan <ahnaf.irfan@stud.th-deg@de>
2025-05-16 16:38:41 +0200

    added calender

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import { Chat } from './pages/chat/chat'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from './context/ThemeContext'
+import { DemoApp } from './components/custom/calender';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <div className="w-full h-screen bg-white dark:bg-gray-900 text-gray-900 dark:text-white">
           <Routes>
             <Route path="/" element={<Chat />} />
+            <Route path="/calender" element={<DemoApp/>} />
           </Routes>
         </div>
       </Router>
